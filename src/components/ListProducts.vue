c97688835b2c6f7d08d2a1fe078e5f70c809e56e
Bruno Galvão Martins <bruno.martins@client-craft.com>
2024-01-11 19:29:11 -0300

    feat(API/PRODUCTS-LIST): create fake json api for server, get products list via fetch and put some styles

---
+++ src/components/ListProducts.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="container mx-auto products__container">
+    <div class="product__wrapper" v-for="product in products" :key="product.id">
+      <img v-if="product.imgs" :src="product.imgs[0].src" :alt="product.imgs[0].title" />
+      <p class="product__price">{{ product.price }}</p>
+      <h2 class="product__title">{{ product.name }}</h2>
+      <p class="product__description">{{ product.description }}</p>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const products = ref('')
+
+const getProducts = () => {
+  fetch('http://localhost:3000/product')
+    .then((r) => r.json())
+    .then((r) => {
+      products.value = r
+    })
+}
+onMounted(() => {
+  getProducts()
+})
+</script>
---
+++ src/views/TheHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Compre ou venda</h1>
+    <SearchProducts />
+    <ListProducts />
+  </section>
+</template>
+
+<script setup>
+import SearchProducts from '@/components/SearchProducts.vue'
+import ListProducts from '@/components/ListProducts.vue'
+</script>
+
+<style scoped>
+h1 {
+  @apply text-3xl text-[#87f] mt-10 text-center font-semibold capitalize;
+}
+</style>
