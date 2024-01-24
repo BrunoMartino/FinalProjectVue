d4f5c518ed69a81b324af872d9b56c563a419df5
Bruno Galvão Martins <bruno.martins@client-craft.com>
2024-01-24 13:57:00 -0300

    feat(SELLER/BUYER): create view for user buyer and user seller and get api in information group

--- src/views/user-pages/UserBuy.vue
+++ src/views/user-pages/UserBuy.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>
-    <h1>Lista de Produtos Comprados</h1>
-  </div>
+  <section>
+    <div v-if="acquisitions">
+      <h2>Compras</h2>
+      <div class="acquisition-wrapper" v-for="(acquisition, index) in acquisitions" :key="index">
+        <ProductItem v-if="acquisition.product" :product="acquisition.product">
+          <p class="seller"><span>Vendedor:</span> {{ acquisition.seller_id }}</p>
+        </ProductItem>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import ProductItem from '@/components/ProductItem.vue'
+import { api } from '@/services.js'
+import { useStore } from '@/stores'
+import { onMounted, ref } from 'vue'
 
-<style></style>
+const store = useStore()
+
+const acquisitions = ref(null)
+
+const getAcquisitions = () => {
+  api.get(`/transaction?buyer_id=${store.user.id}`).then((r) => {
+    acquisitions.value = r.data
+  })
+}
+
+onMounted(() => {
+  if (store.login) {
+    getAcquisitions()
+  }
+})
+</script>
+
+<style scoped>
+.acquisition-wrapper {
+  @apply mb-10;
+}
+
+.seller span {
+  @apply text-[#e80];
+}
+
+h2 {
+  @apply mb-5 text-xl font-semibold;
+}
+</style>
--- src/views/user-pages/UserSell.vue
+++ src/views/user-pages/UserSell.vue
@@ -1,9 +1,68 @@
 <template>
-  <div>
-    <h1>Lista de Produtos Vendidos</h1>
-  </div>
+  <section>
+    <div v-if="sales">
+      <h2>Compras</h2>
+      <div class="acquisition-wrapper" v-for="(sale, index) in sales" :key="index">
+        <ProductItem v-if="sale.product" :product="sale.product">
+          <p class="buyer"><span>Vendedor:</span> {{ sale.seller_id }}</p>
+        </ProductItem>
+        <div class="address">
+          <h3>Endereço de Envio</h3>
+          <ul v-if="sale.address">
+            <li v-for="(value, key) in sale.address" :key="key">{{ key }}: {{ value }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup></script>
+<script setup>
+import ProductItem from '@/components/ProductItem.vue'
+import { api } from '@/services.js'
+import { useStore } from '@/stores'
+import { onMounted, ref } from 'vue'
 
-<style></style>
+const store = useStore()
+
+const sales = ref(null)
+
+const getSales = () => {
+  api.get(`/transaction?seller_id=${store.user.id}`).then((r) => {
+    sales.value = r.data
+  })
+}
+
+onMounted(() => {
+  if (store.login) {
+    getSales()
+  }
+})
+</script>
+
+<style scoped>
+.acquisition-wrapper {
+  @apply mb-10;
+}
+
+.buyer span {
+  @apply text-[#e80];
+}
+
+h2 {
+  @apply mb-5 text-xl font-semibold;
+}
+
+.address {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  gap: 20px;
+  margin-bottom: 60px;
+}
+
+h3 {
+  justify-self: end;
+  margin: 0px;
+  @apply text-lg font-semibold;
+}
+</style>
